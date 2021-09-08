e0ee6f0a4fb36f9d60d621945779713e5516cfd0
Arturo Maldonado <arturo.maldonado@simpat.tech>
2021-09-08 12:07:05 -0500

    End Of Section 9

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Container } from "semantic-ui-react";
+import { Button, Container, Header, Image, Segment } from "semantic-ui-react";
 
 export default function HomePage() {
     return (
-        <Container style={{marginTop: '7em'}}>
-            <h1>Home Page</h1>
-            <h3>Go to <Link to='/activities'>Activities Peludas</Link></h3>
-        </Container>
+        <Segment inverted textAlign='center' vertical className='masthead'>
+            <Container text>
+                <Header>
+                    <Image size='massive' src='/assets/logo.png' alt='logo' style={{ marginBotton: 12 }} />
+                </Header>
+                <Header as='h2' inverted content='Welcome to Pelos duros' />
+                <Button as={Link} to='/activities' size='huge' inverted>
+                    Take OOONNN MEEEEE!
+                </Button>
+            </Container>
+        </Segment>
     )
 }
